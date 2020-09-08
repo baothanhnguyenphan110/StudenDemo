53fd85356310dabad9c20732d412ad79fd4c896a
Administrator <Administrator@Admin>
2020-09-08 09:17:13 +0700

    Version1

---
+++ src/main/java/hello/gitdemo/Student.java
@@ -0,0 +1,14 @@
+
+package hello.gitdemo;
+
+
+public class Student {
+    private int studentId;
+    private String studentName;
+
+    public Student(int studentId, String studentName) {
+        this.studentId = studentId;
+        this.studentName = studentName;
+    }
+    
+}
